f448f380908b86bcc0621beb0246cf52373c770d
Natalie Davidovic <69367552+natalidavid@users.noreply.github.com>
2021-01-16 11:34:11 -0800

    Update breedFetcher.js
    Added comments to help me better understand the material covered

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,13 +5,16 @@ let arg = process.argv[2];
 
 
 const callback = function(error, response, body) {
+  //checking for the error first, then logging the error and returning it
   if (error) {
     console.log(error);
     return;
   }
 
   const data = JSON.parse(body);
+  //parsing the body because otherwise an object
   if (!data.length) {
+    //if data.length === 0, there's nothing inside an array
     console.log("Cat not found");
     return;
   }
@@ -19,13 +22,14 @@ const callback = function(error, response, body) {
   // console.log(typeof data);
   // const catObj = data[0]
   for (cat of data) {
-    console.log('\n', cat.name);
-    console.log(cat.description);
+    console.log('\n', cat.name); //new line, to make it easier to read
+    console.log(cat.description); //accessing only .description value inside an object
   }
   // console.log(catObj.description);
   // console.log(data.length);
 }
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, callback);
+//we coudld've written this callback inline, too
 
 // console.log("We're done!");
